2b287cf59a0e9936cd90f1a906ccbabc7c6c9696
ramaz3004 <ramaz3004@mail.ru>
2024-01-25 18:37:16 +0300

    Добавил третье задание к четвёртому уроку

---
+++ Lesson4(2D array)/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Считать с консоли строку, состояшую из цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+string GetLettersFromString(string s)
+{
+    string Letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        Letters = Letters + e;
+    }
+    return Letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
